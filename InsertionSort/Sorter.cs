b1e7b0b3078e1ac4141f8091c3724046ad9f37b9
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 22:04:42 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ InsertionSort/Sorter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+namespace InsertionSort
+{
+    public static class Sorter
+    {
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with insertion sort algorithm.
+        /// </summary>
+        public static void InsertionSort(this int[]? array)
+        {
+            // TODO #1. Implement the method using a loop statements.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with recursive insertion sort algorithm.
+        /// </summary>
+        public static void RecursiveInsertionSort(this int[]? array)
+        {
+            // TODO #2. Implement the method using recursion algorithm.
+            throw new NotImplementedException();
+        }
+    }
+}
