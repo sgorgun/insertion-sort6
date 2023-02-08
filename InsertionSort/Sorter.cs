901efdae4be2d2b826386a7a1a86b9f24f629b7d
@gorgun.sergei <gorgun.sergei@student.ehu.lt>
2023-02-08 23:19:14 +0200

    Ready

--- InsertionSort/Sorter.cs
+++ InsertionSort/Sorter.cs
@@ -10,8 +10,30 @@ namespace InsertionSort
         /// </summary>
         public static void InsertionSort(this int[]? array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), $"{nameof(array)} is null");
+            }
+
+            if (array == Array.Empty<int>())
+            {
+                throw new ArgumentException($"{nameof(array)} is empty.", nameof(array));
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int sorted = i - 1;
+                while (sorted > -1 && array[sorted] > array[sorted + 1])
+                {
+                    Swap(ref array[sorted], ref array[sorted + 1]);
+                    sorted--;
+                }
+            }
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            (a, b) = (b, a);
         }
 
         /// <summary>
@@ -19,8 +41,36 @@ namespace InsertionSort
         /// </summary>
         public static void RecursiveInsertionSort(this int[]? array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), $"{nameof(array)} is null");
+            }
+
+            if (array == Array.Empty<int>())
+            {
+                throw new ArgumentException($"{nameof(array)} is empty.", nameof(array));
+            }
+
+            InsertionSortRecursive(array, 0);
+        }
+
+        public static void InsertionSortRecursive(this int[]? array, int i = 1)
+        {
+            if (i == array!.Length)
+            {
+                return;
+            }
+
+            int j = i - 1;
+            if (j >= 0 && array[j] > array[j + 1])
+            {
+                Swap(ref array[j], ref array[j + 1]);
+                InsertionSortRecursive(array, i - 1);
+            }
+            else
+            {
+                InsertionSortRecursive(array, i + 1);
+            }
         }
     }
 }
